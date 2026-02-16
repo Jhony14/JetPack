634576e4fab7218109a867ed500ad8f30cbe4299
calonator87 <beltran8769@gmail.com>
2026-02-16 22:29:59 +0100

    libreria grafica

---
+++ jugador.cc
@@ -0,0 +1,67 @@
+// ESAT Libraries
+#include <esat/window.h>
+#include <esat/draw.h>
+#include <esat/sprite.h>
+#include <esat/input.h>
+#include <esat/time.h>
+
+// Standard libraries
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <math.h>
+
+const int KWindow_Width = 800;
+const int KWindow_Height = 800;
+
+// FPS
+double delta_time;
+unsigned char fps = 25;
+double current_time;
+double last_time;
+
+struct Jugador
+{
+  esat::Vec2 pos;
+};
+
+int esat::main(int argc, char **argv)
+{
+
+  esat::WindowInit(KWindow_Width, KWindow_Height);
+  esat::WindowSetMouseVisibility(true);
+
+  srand((unsigned)time(nullptr));
+  last_time = esat::Time();
+
+  // Main game loop
+  while (esat::WindowIsOpened() && !esat::IsSpecialKeyDown(esat::kSpecialKey_Escape))
+  {
+
+    // Calculate time elapsed since the last frame
+    current_time = esat::Time();
+    delta_time = (current_time - last_time) / 1000.0;
+    // Limit delta_time to avoid large jumps
+    if (delta_time > 0.1)
+      delta_time = 0.1;
+
+    last_time = current_time;
+
+    esat::DrawBegin();
+    esat::DrawClear(0, 0, 0);
+
+    // Finish drawing
+    esat::DrawEnd();
+    // Control frame speed (fps)
+    do
+    {
+      current_time = esat::Time();
+    } while ((current_time - last_time) <= 1000.0 / fps);
+    esat::WindowFrame();
+  }
+
+  // Destroy window
+  esat::WindowDestroy();
+  return 0;
+}
