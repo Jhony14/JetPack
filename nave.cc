3d1b6cf0b1a9ddc91fd29bf42c204ca8695bb449
garciavi <garciavi@esat-alumni.com>
2026-02-18 20:31:19 +0100

    nave mov

--- nave.cc
+++ nave.cc
@@ -12,7 +12,6 @@
 #include <time.h>
 #include <math.h>
 
-
 // Windows dimensions with default values
 const int KWindow_Width = 800;
 const int KWindow_Height = 800;
@@ -23,61 +22,127 @@ unsigned char fps = 25;
 double current_time;
 double last_time;
 
-esat::SpriteHandle nave1, nave2, nave3;
-esat::SpriteHandle rosa;
+enum Direction{
+  UP,
+  DOWN,
+};
 
-int g_pink_x, g_pink_y;
-int g_head_x = 600, g_head_y = 704;
-int g_body_x = 600, g_body_y = 736;
-int g_tail_x = 600, g_tail_y = 768;
-int g_speed = 2;
+struct Sprite{
+  esat::SpriteHandle sprite;
+};
 
+struct Nave{
+    float vel;
+    esat::Vec2 pos;
+    Direction direccion;
+    bool activada;
+    int height = 32;
+};
 
-void CargarSprites(){
-    nave1 = esat::SpriteFromFile("./SPRITES/NAVE/cabeza_nave_1_2x.png");
-    nave2 = esat::SpriteFromFile("./SPRITES/NAVE/cuerpo_nave_1_2x.png");
-    nave3 = esat::SpriteFromFile("./SPRITES/NAVE/cola_nave_1_2x.png");
-    rosa = esat::SpriteFromFile("rosa.png");
+Sprite *InstanciarSpritesNave(int numSprite){
+  return (Sprite*)malloc(sizeof(Sprite)*numSprite);
 }
-void Dibujar(){
-    esat::DrawSprite(rosa, g_head_x, g_head_y);
-    esat::DrawSprite(nave1, g_head_x, g_head_y);
 
-    esat::DrawSprite(rosa, g_body_x, g_body_y);
-    esat::DrawSprite(nave2, g_body_x, g_body_y);
-
-    esat::DrawSprite(rosa, g_tail_x, g_tail_y);
-    esat::DrawSprite(nave3, g_tail_x, g_tail_y);
-
+void InitSpriteNave(Sprite *punteroSprites)
+{
+  punteroSprites[0].sprite = esat::SpriteFromFile("./SPRITES/NAVE/cola_nave_1_2x.png");
+  punteroSprites[1].sprite = esat::SpriteFromFile("./SPRITES/NAVE/cuerpo_nave_1_2x.png");
+  punteroSprites[2].sprite = esat::SpriteFromFile("./SPRITES/NAVE/cabeza_nave_1_2x.png");
+  punteroSprites[3].sprite = esat::SpriteFromFile("./SPRITES/NAVE/cola_nave_2_2x.png");
+  punteroSprites[4].sprite = esat::SpriteFromFile("./SPRITES/NAVE/cuerpo_nave_2_2x.png");
+  punteroSprites[5].sprite = esat::SpriteFromFile("./SPRITES/NAVE/cabeza_nave_2_2x.png");
+  punteroSprites[6].sprite = esat::SpriteFromFile("./SPRITES/NAVE/cola_nave_3_2x.png");
+  punteroSprites[7].sprite = esat::SpriteFromFile("./SPRITES/NAVE/cuerpo_nave_3_2x.png");
+  punteroSprites[8].sprite = esat::SpriteFromFile("./SPRITES/NAVE/cabeza_nave_3_2x.png");
+  punteroSprites[9].sprite = esat::SpriteFromFile("./SPRITES/NAVE/cola_nave_4_2x.png");
+  punteroSprites[10].sprite = esat::SpriteFromFile("./SPRITES/NAVE/cuerpo_nave_4_2x.png");
+  punteroSprites[11].sprite = esat::SpriteFromFile("./SPRITES/NAVE/cabeza_nave_4_2x.png");
+  punteroSprites[12].sprite = esat::SpriteFromFile("./SPRITES/NAVE/fondo_blanco.png");
+  punteroSprites[13].sprite = esat::SpriteFromFile("./SPRITES/NAVE/fondo_rosa.png");
+  punteroSprites[14].sprite = esat::SpriteFromFile("./SPRITES/NAVE/explosion_1_2x.png");
+  punteroSprites[15].sprite = esat::SpriteFromFile("./SPRITES/NAVE/explosion_2_2x.png");
 }
-void MoverNave(){
-  if(esat::IsKeyDown ('P')){
-    while (g_head_y != 100)
-    {
-          g_head_y -= g_speed;
-          g_body_y -= g_speed;
-          g_tail_y -= g_speed;
-    }
-    
 
+void InstanciarNave(Nave *nave)
+{
+  nave->activada = false;
+  nave->vel = 10;
+  nave->pos.x = KWindow_Width/2;
+  nave->pos.y = KWindow_Height - nave->height;
+}
+
+void DibujarNave(Nave *nave, Sprite *punteroSprites){
+//  esat::DrawSprite(punteroSprites[12].sprite, nave->pos.x-100, nave->pos.y-(nave->height));
+//   esat::DrawSprite(punteroSprites[14].sprite, nave->pos.x-100, nave->pos.y + (nave->height));
+
+  for(int i=0; i<3; i++){
+      // for(int j=0; j<2; j++){
+      //   int altFuel = 16;
+        
+      //   esat::DrawSprite(punteroSprites[13].sprite, nave->pos.x, KWindow_Height - (altFuel*j));
+      // }
+      esat::DrawSprite(punteroSprites[12].sprite, nave->pos.x, nave->pos.y-(nave->height*i));
+      esat::DrawSprite(punteroSprites[i].sprite, nave->pos.x, nave->pos.y-(nave->height*i));
+  }
+ 
+}
+
+void DibujarFuel(Nave *nave, Sprite *punteroSprites){
+  int altFuel = 16, contFuel = 1;
+  int posY = KWindow_Height-altFuel;
+
+  for(int i=0; i<6; i++){
+    esat::DrawSprite(punteroSprites[13].sprite, nave->pos.x, posY);
+    contFuel++;
+    posY = KWindow_Height - (altFuel*contFuel);
+  }
+}
+
+void TriggerNave(Nave *nave, Sprite *punteroSprites){
+  if(esat::IsSpecialKeyDown(esat::SpecialKey::kSpecialKey_Up)){
+    nave->activada = true;
+    nave->direccion = Direction::UP;
   }
 
+  //if(esat::IsSpecialKeyDown(esat::SpecialKey::kSpecialKey_Space)){
 
+  //}
+}
+
+void MoverNave(Nave *nave){
+  switch (nave->direccion){
+    case Direction::UP: nave->pos.y -= nave->vel;
+    break;
+
+    case Direction::DOWN: nave->pos.y += nave->vel;
+    break;
+  }
+
+  if(nave->pos.y <= 100){
+     nave->direccion = Direction::DOWN;
+  }
+
+  if(nave->pos.y + nave->height >= KWindow_Height){
+    nave->pos.y = KWindow_Height - nave->height;
+  }
 
 }
-void Liberar(){
-    esat::SpriteRelease(nave1);
-    esat::SpriteRelease(nave2);
-    esat::SpriteRelease(nave2);
-    esat::SpriteRelease(rosa);
-}
+
 int esat::main(int argc, char **argv) {
 
 	esat::WindowInit(KWindow_Width, KWindow_Height);
 	esat::WindowSetMouseVisibility(true);
-    CargarSprites();
+
 	srand((unsigned)time(nullptr));
 	last_time = esat::Time();
+
+  Sprite *punteroSprites = InstanciarSpritesNave(16);
+  InitSpriteNave(punteroSprites);
+
+  Nave nave;
+  InstanciarNave(&nave);
+
+ 
 
   // Main game loop
   while(esat::WindowIsOpened() && !esat::IsSpecialKeyDown(esat::kSpecialKey_Escape)) {
@@ -89,12 +154,13 @@ int esat::main(int argc, char **argv) {
     if (delta_time > 0.1) delta_time = 0.1;
     
     last_time = current_time;
-  
+
     esat::DrawBegin();
     esat::DrawClear(0,0,0);
 
-    Dibujar();
-    MoverNave();
+    DibujarNave(&nave, punteroSprites);
+    TriggerNave(&nave, punteroSprites);
+    MoverNave(&nave);
 
     // Finish drawing
     esat::DrawEnd();
@@ -104,9 +170,8 @@ int esat::main(int argc, char **argv) {
     }while((current_time - last_time) <= 1000.0/fps);
     esat::WindowFrame();
   }
-    
+
   // Destroy window
-  Liberar();
   esat::WindowDestroy();
   return 0;
 }
