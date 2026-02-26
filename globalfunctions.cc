8ef54281996d861bc82a663a5ff3766456f2dbf0
Robertic75 <simbanar90@gmail.com>
2026-02-26 18:52:49 +0100

    sds

--- globalfunctions.cc
+++
@@ -1,8 +0,0 @@
-#include "globalfunctions.h"
-#include <stdlib.h>
-
-namespace GLO {
-    void* AllocateMemory(int num_of_elements, size_t size_of_elements) {
-        return malloc(num_of_elements * size_of_elements);
-    }
-}
--- globalfunctions.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace GLO{
-    /*Crea un bloque de memoria de tipo void para después castearlo a cualquier tipo de puntero
-        param num_of_elements Cuántos elementos debe tener el bloque de memoria
-        param size_of_elements Un sizeof del tipo de dato que va a guardar el puntero
-    */
-    void* AllocateMemory(int num_of_elements, size_t size_of_elements);
-}
