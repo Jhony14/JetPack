ac24bc9e788ccb9b70170af5eaae50ba26694ad3
Jhony14 <jhonyart14@gmail.com>
2026-02-27 23:34:22 +0100

    - Fix player instantiation for each new game.

--- merge.cc
+++ merge.cc
@@ -84,16 +84,16 @@ void InitiateAll(Sprites **spritesColores, Sprites **spritesPersonaje, Bala **pu
     ENE::InitVFXSystem(g_fx_pool, g_fx_sprites);
 }
 
-void GetInput(bool *moverLeft, bool *moverRight, bool *ascender, Bala *punteroBalas, Jugador player,
+void GetInput(bool *moverLeft, bool *moverRight, bool *ascender, Bala *punteroBalas, Jugador *player,
              TGame* game, int* menu_selection_player, int* menu_selection_control)
 {
     if(game->current_screen == TScreen::GAME_SCREEN){
-        if (!player.muerto)
+        if (!player->muerto)
         {
             *moverLeft = (esat::IsKeyPressed('A') || esat::IsKeyPressed('a'));
             *moverRight = (esat::IsKeyPressed('D') || esat::IsKeyPressed('d'));
             *ascender = (esat::IsKeyPressed('W') || esat::IsKeyPressed('w'));
-            CrearDisparos(punteroBalas, player);
+            CrearDisparos(punteroBalas, *player);
         }
     }
     if(game->current_screen == TScreen::MAIN_MENU){
@@ -102,17 +102,18 @@ void GetInput(bool *moverLeft, bool *moverRight, bool *ascender, Bala *punteroBa
       if (esat::IsKeyPressed('3')) *menu_selection_control = 0;
       if (esat::IsKeyPressed('4')) *menu_selection_control = 1;
       if (esat::IsKeyPressed('5')) {
+        InstanciarPlayer(player); // Instanciamos siempre el jugador 1
         if (*menu_selection_player == 1) {
-          // Save player 2 data
+          // Instanciamos el jugador 2 y lo guardamos en el archivo
           Jugador player2;
           InstanciarPlayer(&player2);
           player2.player_id = 2;
           player2.isActive = false;
           player2.muerto = true;
           player2.colisiona = false;
-          SavePlayerDataToFile(&player, &player2);
+          SavePlayerDataToFile(player, &player2);
         }else{
-          SavePlayerDataToFile(&player);
+          SavePlayerDataToFile(player);
         }
         game->current_screen = TScreen::GAME_SCREEN;
       }
@@ -307,7 +308,7 @@ int esat::main(int argc, char **argv)
     {
         InitiateFrame();
 
-        GetInput(&moverLeft, &moverRight, &ascender, punteroBalas, player, &game, &menu_selection_player, &menu_selection_control);
+        GetInput(&moverLeft, &moverRight, &ascender, punteroBalas, &player, &game, &menu_selection_player, &menu_selection_control);
         Update(&player, &ascender, punteroBalas, &moverLeft, &moverRight, &frame, gasofa, &itemdrop, spritesItems, g_platforms, &game, &timer,
                 &menu_blink_timer, &menu_highlight_white, &nave, enemies, level, parteNave, g_fx_pool, g_fx_sprites);
         DrawAll(spritesColores, spritesPersonaje, punteroBalas, player, frame, gasofa, spritesItems, itemdrop, g_platforms, platform_sprite, 
